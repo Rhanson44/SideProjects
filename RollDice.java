317333467e1910f10573dc80b9685be719fce9fb
Ryan Hanson <hansonrj8322@gmail.com>
2024-06-09 12:31:31 -0600

    rolldice

---
+++ RollDice.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class RollDice{
+
+	public static void main (String [] args) {
+
+		Random rand = new Random();
+		int x = rand.nextInt(6) + 1;
+
+		System.out.println("You rolled a " + x);
+	}
+}
