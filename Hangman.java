59b7aba40b1a00c0e19b04e9e957e9814d798bf5
Ryan Hanson <hansonrj8322@gmail.com>
2024-06-11 14:10:08 -0600

    addlives

--- Hangman.java
+++ Hangman.java
@@ -6,6 +6,7 @@ public class Hangman {
 	public static void main(String [] args) {
 		Scanner scan = new Scanner(System.in);
 		Random rand = new Random();
+		int lives = 7;
 
 		String wordList[] = {"apple", "balloon", "chalk", "drought", "elephant", "fright", "gigantic"};
 		int randomWord = rand.nextInt(wordList.length);
@@ -16,10 +17,11 @@ public class Hangman {
 		for (int i = 0; i < word.length(); i++){
 			hideWord.append("_");
 		}
-
 		System.out.println(hideWord);
 
-		while (hideWord.toString().contains("_")) {
+		System.out.println("Guesses left: " + lives);
+
+		while (hideWord.toString().contains("_") && lives > 0) {
 
 			System.out.println("Guess a character");
 			char input = scan.next().charAt(0);
@@ -37,9 +39,10 @@ public class Hangman {
 				System.out.println("correct"); 
 			} else {
 				System.out.println("incorrect");
+				lives -= 1;
 			}
-
 			System.out.println(hideWord);
+			System.out.println("Guesses left: " + lives);
 		}
 		scan.close();
 	}
